1ac3ac971e6ddbd7f65f21580de9ce436bdc7c79
don-cat <mamayanyu@hotmail.com>
2017-02-11 00:40:04 +0800

    增加支出与收入分类

--- src/model/EnumExpenseType.java
+++ src/model/EnumExpenseType.java
@@ -4,19 +4,31 @@ package model;
  * @author mamay
  *
  */
-public class EnumExpenseType {
-	public enum dailyET{
-		/*
-		 * 日常生活账本默认支出类型
-		 */
-		CLOTHING,FOOD,TRANSPORT,LIVING,INTERTAINMENT,STUDY,HEALTH
-	}
+public enum EnumExpenseType {
 	
-	public enum travelET{
+		/*
+		 * 支出类型首位为2，收入类型首位为1
+		 */
+//		CLOTHING,FOOD,TRANSPORT,LIVING,INTERTAINMENT,STUDY,HEALTH
+		CLOTHING("服饰",201),
+		FOOD("三餐",202),
+		TRANSPORT("交通",202),
+		LIVING("住宿",202),
+		INTERTAINMENT("娱乐",202),
+		STUDY("学习",202),
+		SPORT("运动",202);
+		
+		private String dailyType;
+		private int code;
+		
+		private EnumExpenseType(String dailyType,int code){
+			this.dailyType = dailyType;
+			this.code = code;
+		}
+	
 		/*
 		 * 旅游账本默认支出类型
 		 */
-		TRANSPORT,ACCOMMODATION,ADMISSIONTICKET,FOOD,PASSBOOK
-	}
+//		TRANSPORT,ACCOMMODATION,ADMISSIONTICKET,FOOD,PASSBOOK
 	
 }
---
+++ src/service/GetExpenseType.java
@@ -0,0 +1,9 @@
+package service;
+
+import Servlet.GetEnumModule;
+
+public class GetExpenseType {
+	public list expenseType(String userid){
+		GetEnumModule[] type = GetEnumModule.values();
+	}
+}
